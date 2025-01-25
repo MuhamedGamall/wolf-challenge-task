ecae9b2c031efbead6f895e0c5dee2e7ad26d335
MuhamedGamall <muhamedGamal250@gmail.com>
2025-01-25 15:45:16 +0200

    feat: add validation schemas for course creation using Zod and Yup, and implement text truncation utility

---
+++ src/lib/formSchema.ts
@@ -0,0 +1,32 @@
+import * as z from "zod";
+import * as y from "yup";
+
+export const createCourseSchemaWithZod = (t: any) =>
+  z.object({
+    title: z
+      .string()
+      .trim()
+      .min(4, { message: t("inputErrorMessages.title.min") })
+      .max(100, { message: t("inputErrorMessages.title.max") }),
+    description: z
+      .string()
+      .trim()
+      .max(500, { message: t("inputErrorMessages.description.max") }),
+    image: z.string().refine((value) => value !== "", {
+      message: t("inputErrorMessages.image.required"),
+    }),
+    video: z.string().refine((value) => value !== "", {
+      message: t("inputErrorMessages.video.required"),
+    }),
+  });
+
+export const createCourseSchemaWithYup = (t: any) =>
+  y.object().shape({
+    title: y.string()
+      .required()
+      .min(4, t("inputErrorMessages.title.min"))
+      .max(100, t("inputErrorMessages.title.max")),
+    description: y.string().max(500, t("inputErrorMessages.description.max")),
+    image: y.string().required(t("inputErrorMessages.image.required")),
+    video: y.string().required(t("inputErrorMessages.video.required")),
+  });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+export const truncateText = (text: string, limit: number) => {
+  return text?.length > limit ? text?.substring(0, limit) + "..." : text;
+};
